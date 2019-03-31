eb8bba404574861fc182349b21e234b2d1f5ba80
Rafael Camilo <camilo.melges@gmail.com>
2019-03-30 20:06:23 -0400

    webpack conf for sass loader, added some packages for this

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
       { test: /\.js$/, use: 'babel-loader' },
       { test: /\.vue$/, use: 'vue-loader' },
       { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+      { test: /\.s[a|c]ss$/, use: ['style-loader', 'vue-style-loader', 'css-loader', 'sass-loader']}
     ]
   },
   plugins: [
