456d9941aa7fff2718b4fa5f45e2df13072511e9
Rafael Camilo <camilo.melges@gmail.com>
2019-03-31 02:13:35 -0400

    stylized itens and almost finished home view component

--- src/Components/Home.vue
+++ src/Components/Home.vue
@@ -1,27 +1,52 @@
 <template>
-    <div>
-        Home
-    </div>
+  <div class="comunicates">
+    <div
+      class="comunicate"
+      v-bind:key="comunicate.id"
+      v-for="comunicate in comunicates"
+      v-bind:style="colorBorder(comunicate.type)"
+    >{{ comunicate.title }}</div>
+  </div>
 </template>
 <script>
+import { comunicados, tiposComunicados } from "../../endpoints/db.json";
 export default {
-    
-}
+  data() {
+    return {
+      comunicates: comunicados,
+      colorBorder(type) {
+      switch (type) {
+        case 3:
+          return {borderLeft: '5px solid #FF9900'}
+          break;
+        case 2:
+          return {borderLeft: '5px solid #56CCF2'}
+          break;
+        case 1:
+          return {borderLeft: '5px solid #79C126'}
+          break;
+        default:
+          return {borderLeft: '5px solid #660099'}
+          break;
+      }
+    }
+    };
+  }
+};
 </script>
 
-<style scoped>
-    /* @keyframes example {
-      0%   {background-color: red;}
-      25%  {background-color: yellow;}
-      50%  {background-color: blue;}
-      100% {background-color: green;}
-    }
-    
-    div {
-      width: 100%;
-      height: 100%;
-      background-color: red;
-      animation-name: example;
-      animation-duration: 4s;
-    } */
+<style lang="scss" scoped>
+.comunicates {
+  padding-left: 15px;
+  padding-right: 15px;
+  .comunicate {
+    border-radius: 0.2em;
+    background-color: #ffff;
+    padding: 10px;
+    margin-bottom: 20px;
+    -webkit-box-shadow: -1px 6px 10px -2px #edecee;
+    -moz-box-shadow: -1px 6px 10px -2px #edecee;
+    box-shadow: -1px 6px 10px -2px #edecee;
+  }
+}
 </style>
